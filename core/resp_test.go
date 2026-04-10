5ec77347f6061f975fe9ad5d256989cc6effdc61
Ardit Zubaku <zubaku92@gmail.com>
2026-04-10 22:43:30 +0200

    simple error - RESP decoding + tests

--- core/resp_test.go
+++ core/resp_test.go
@@ -10,13 +10,23 @@ func TestSimpleStringDecode(t *testing.T) {
 	cases := map[string]string{
 		"+OK\r\n": "OK",
 	}
+
+	execCases(t, cases)
+}
+
+func TestError(t *testing.T) {
+	cases := map[string]string{
+		"-Error message\r\n": "Error message",
+	}
+
+	execCases(t, cases)
+}
+
+func execCases(t *testing.T, cases map[string]string) {
 	for k, v := range cases {
 		res, _ := core.Decode([]byte(k))
 		if res != v {
 			t.Fail()
 		}
 	}
-}
-
-func TestError(t *testing.T) {
 }
