a37a5213c3f10d2e4bebd651d455f23bcfd6a210
Ardit Zubaku <zubaku92@gmail.com>
2026-04-12 10:51:32 +0200

    standardize error messages for Redis commands in errors.go

--- core/errors.go
+++ core/errors.go
@@ -11,6 +11,10 @@ var (
 	ErrMissingCRLF         = errors.New("invalid simple string: missing CRLF")
 	ErrIntegerOverflow     = errors.New("integer overflow")
 	ErrInvalidType         = errors.New("invalid type")
+
 	// This is the exact error message Redis returns
-	ErrPingInvalidArgs = errors.New("ERR wrong number of arguments for 'PING' command")
+	ErrPingInvalidArgs      = errors.New("ERR wrong number of arguments for 'ping' command")
+	ErrSetInvalidArgs       = errors.New("(error) ERR wrong number of arguments for 'set' command")
+	ErrSyntaxError          = errors.New("(error) ERR syntax error")
+	ErrNotIntegerOutOfRange = errors.New("(error) ERR value is not an integer or out of range")
 )
